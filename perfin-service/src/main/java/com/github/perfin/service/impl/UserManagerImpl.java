a320136016420a4c16918970b961605d3e54e318
Tomáš Livora <livoratom@gmail.com>
2015-06-21 19:07:36 +0200

    Implement getCurrentUser() method using principal name

--- perfin-service/src/main/java/com/github/perfin/service/impl/UserManagerImpl.java
+++ perfin-service/src/main/java/com/github/perfin/service/impl/UserManagerImpl.java
@@ -1,34 +1,39 @@
 package com.github.perfin.service.impl;
 
-import javax.annotation.security.PermitAll;
-import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import com.github.perfin.model.entity.Currency;
 import com.github.perfin.model.entity.User;
 import com.github.perfin.service.api.UserManager;
 
+import javax.annotation.security.PermitAll;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.security.Principal;
 
 @Stateless
 @PermitAll
 public class UserManagerImpl implements UserManager {
-	
-	@PersistenceContext
-	private EntityManager em;
 
-	@Override
-	public void changeDefaultCurrency(Currency currency) {
-		User user = em.find(User.class, getCurrentUser().getId());
-		user.setDefaultCurrency(currency);
-		
-		em.merge(user);
-		
-	}
+    @PersistenceContext
+    private EntityManager em;
 
-	@Override
-	public User getCurrentUser() {
-		throw new UnsupportedOperationException("unspported yet");
-	}
+    @Inject
+    private Principal principal;
+
+    @Override
+    public void changeDefaultCurrency(Currency currency) {
+        User user = em.find(User.class, getCurrentUser().getId());
+        user.setDefaultCurrency(currency);
+        em.merge(user);
+    }
+
+    @Override
+    public User getCurrentUser() {
+        Query query = em.createQuery("SELECT u FROM User u WHERE u.userName = :name", User.class);
+        query.setParameter("name", principal.getName());
+        return (User) query.getSingleResult();
+    }
 
 }
