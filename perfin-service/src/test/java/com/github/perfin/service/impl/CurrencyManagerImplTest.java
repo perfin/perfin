a157da015d7edcfe54b7ef383afff28765441138
Tomáš Livora <livoratom@gmail.com>
2015-06-21 11:42:58 +0200

    Fix exception types in CurrencyManagerImplTest

--- perfin-service/src/test/java/com/github/perfin/service/impl/CurrencyManagerImplTest.java
+++ perfin-service/src/test/java/com/github/perfin/service/impl/CurrencyManagerImplTest.java
@@ -1,21 +1,9 @@
 package com.github.perfin.service.impl;
 
-import static org.assertj.core.api.Assertions.*;
-
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.List;
-
-import javax.ejb.EJBException;
-import javax.inject.Inject;
-import javax.transaction.RollbackException;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
+import com.github.perfin.model.entity.Currency;
+import com.github.perfin.service.api.CurrencyManager;
+import com.github.perfin.service.dto.PaginatedListWrapper;
+import com.github.perfin.service.rest.ExchangeRatesProvider;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
@@ -28,84 +16,96 @@ import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import com.github.perfin.model.entity.Currency;
-import com.github.perfin.service.api.CurrencyManager;
-import com.github.perfin.service.dto.PaginatedListWrapper;
-import com.github.perfin.service.rest.ExchangeRatesProvider;
+import javax.ejb.EJBException;
+import javax.inject.Inject;
+import javax.persistence.PersistenceException;
+import javax.validation.ConstraintViolationException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
 @RunWith(Arquillian.class)
 public class CurrencyManagerImplTest {
 
-	@Inject
-	private CurrencyManager currencyManager;
-	
-	@Deployment
+    @Inject
+    private CurrencyManager currencyManager;
+
+    @Deployment
     public static Archive<?> getDeployment() {
         WebArchive war = ShrinkWrap
                 .create(WebArchive.class)
                 .addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
-                .addPackages(true, 
-            		CurrencyManager.class.getPackage(), 
-            		CurrencyManagerImpl.class.getPackage(), 
-            		Currency.class.getPackage(),
-            		ExchangeRatesProvider.class.getPackage(),
-            		PaginatedListWrapper.class.getPackage()).
-        		addPackages(true, "org.assertj.core");
-        
+                .addPackages(true,
+                        CurrencyManager.class.getPackage(),
+                        CurrencyManagerImpl.class.getPackage(),
+                        Currency.class.getPackage(),
+                        ExchangeRatesProvider.class.getPackage(),
+                        PaginatedListWrapper.class.getPackage()).
+                        addPackages(true, "org.assertj.core");
+
         war.addAsLibraries(Maven.resolver().loadPomFromFile("pom.xml")
                 .resolve("org.mockito:mockito-all").withTransitivity().asFile());
-        
+
         war.addAsLibraries(Maven.resolver().loadPomFromFile("pom.xml")
                 .resolve("org.apache.commons:commons-lang3").withTransitivity().asFile());
-        
+
         return war;
     }
 
     @After
     public void clearRecords() {
-        if(currencyManager != null) {
-        List<Currency> currencies = currencyManager.getCurrencies(1, "id", "asc").getList();
-        for(Currency c : currencies) {
-        currencyManager.deleteCurrency(c.getId());
+        if (currencyManager != null) {
+            List<Currency> currencies = currencyManager.getCurrencies(1, "id", "asc").getList();
+            for (Currency c : currencies) {
+                currencyManager.deleteCurrency(c.getId());
             }
         }
     }
-    
+
     @Test
     public void testSaveGetDelete() {
-    	Currency currencyOne = new Currency();
-    	currencyOne.setCode("ABC");
-    	currencyOne.setName("ABECE");
-    	currencyOne = currencyManager.saveCurrency(currencyOne);
-    	assertThat(currencyOne.getId()).isNotNull();
-    	
-    	Currency currencyTwo = new Currency();
+        Currency currencyOne = new Currency();
+        currencyOne.setCode("ABC");
+        currencyOne.setName("ABECE");
+        currencyOne = currencyManager.saveCurrency(currencyOne);
+        assertThat(currencyOne.getId()).isNotNull();
+
+        Currency currencyTwo = new Currency();
         currencyTwo.setCode("DEF");
         currencyTwo.setName("DEEF");
         currencyTwo = currencyManager.saveCurrency(currencyTwo);
         assertThat(currencyTwo.getId()).isNotNull();
-        
+
         currencyManager.deleteCurrency(currencyOne.getId());
         PaginatedListWrapper<Currency> wrapper = currencyManager.getCurrencies(1, "id", "asc");
         assertThat(wrapper.getList().size()).isEqualTo(1);
         assertThat(wrapper.getList().get(0)).isEqualTo(currencyTwo);
     }
-    
+
     @Test
     public void testUpdate() {
         Currency currency = new Currency();
         currency.setCode("ABC");
         currency = currencyManager.saveCurrency(currency);
-        
+
         Currency updated = currency;
         updated.setName("Not Nulll Name");
         updated = currencyManager.saveCurrency(updated);
-        
+
         PaginatedListWrapper<Currency> wrapper = currencyManager.getCurrencies(1, "id", "asc");
         assertThat(wrapper.getList().size()).isEqualTo(1);
         assertThat(wrapper.getList().get(0)).isEqualTo(updated);
     }
-    
+
     @Test
     public void testInvalidOperations() {
         // invalid code
@@ -114,66 +114,66 @@ public class CurrencyManagerImplTest {
         try {
             currencyManager.saveCurrency(currency);
             fail("Code is not uppercase ond of length 3");
-        } catch(EJBException ejbe) { 
-            assertThat(ejbe.getCause()).isInstanceOf(RollbackException.class);
+        } catch (EJBException ejbe) {
+            assertThat(ejbe.getCause()).isInstanceOf(ConstraintViolationException.class);
         }
-        
+
         currency.setCode(null);
         try {
             currencyManager.saveCurrency(currency);
             fail("Code is not uppercase ond of length 3");
-        } catch(EJBException ejbe) { 
-            assertThat(ejbe.getCause()).isInstanceOf(RollbackException.class);
+        } catch (EJBException ejbe) {
+            assertThat(ejbe.getCause()).isInstanceOf(ConstraintViolationException.class);
         }
-        
+
         currency.setCode("XXX");
         currencyManager.saveCurrency(currency);
-        
+
         Currency nonUnique = new Currency();
         nonUnique.setCode("XXX");
         try {
             currencyManager.saveCurrency(nonUnique);
             fail("stored non unique code");
-        } catch(EJBException ejbe) { 
-            assertThat(ejbe.getCause()).isInstanceOf(RollbackException.class);
+        } catch (EJBException ejbe) {
+            assertThat(ejbe.getCause()).isInstanceOf(PersistenceException.class);
         }
-        
+
         try {
             currencyManager.deleteCurrency(123456l);
             fail("fail non existing record");
-        }catch(EJBException e) {
+        } catch (EJBException e) {
             //ok
         }
     }
-	
+
     @Test
     @RunAsClient
     public void testRest(@ArquillianResource URL base) throws URISyntaxException {
         Currency unstored = new Currency();
         unstored.setCode("QWE");
-        
+
         Entity<Currency> currency = Entity.entity(unstored, MediaType.APPLICATION_JSON);
-        
+
         Client client = ClientBuilder.newClient();
         WebTarget target = client.target(base.toURI() + "resources/currencies/");
         Response response = target.request(MediaType.APPLICATION_JSON).post(currency);
-        
+
         Currency stored = response.readEntity(Currency.class);
         assertThat(stored.getId()).isNotNull();
         assertThat(stored.getCode()).isEqualTo(unstored.getCode());
         assertThat(stored.getName()).isEqualTo(unstored.getName());
-        
+
         Response responseGet = target.request(MediaType.APPLICATION_JSON).get();
         PaginatedListWrapper<Currency> wrapper = responseGet.readEntity(PaginatedListWrapper.class);
         assertThat(wrapper.getList().size()).isEqualTo(1);
-        
-        WebTarget targetDelete = client.target(base.toURI() + "resources/currencies/" 
+
+        WebTarget targetDelete = client.target(base.toURI() + "resources/currencies/"
                 + stored.getId() + "/");
         targetDelete.request().delete();
-        
+
         responseGet = target.request(MediaType.APPLICATION_JSON).get();
         wrapper = responseGet.readEntity(PaginatedListWrapper.class);
         assertThat(wrapper.getList().size()).isEqualTo(0);
-	    
     }
+
 }
