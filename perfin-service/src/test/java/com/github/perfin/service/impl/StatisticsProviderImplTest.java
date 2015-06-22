35a982d16aad967e8ecb9ea50722b84a23066ea1
Marián Macik <macik.marian@gmail.com>
2015-06-22 22:17:24 +0200

    Updated @Before method in statistics test.

--- perfin-service/src/test/java/com/github/perfin/service/impl/StatisticsProviderImplTest.java
+++ perfin-service/src/test/java/com/github/perfin/service/impl/StatisticsProviderImplTest.java
@@ -24,17 +24,8 @@ public class StatisticsProviderImplTest extends TestCase {
     @PersistenceContext
     private EntityManager em;
 
-    @Inject
+    @Mock
     private UserManager userManager;
-
-    @Inject
-    private CurrencyManager currencyManager;
-
-    @Inject
-    private CurrencyConverter currencyConverter;
-
-    @Inject
-    private ExchangeRatesProvider exchangeRatesProvider;
 
     private Currency EUR;
 
@@ -75,6 +66,11 @@ public class StatisticsProviderImplTest extends TestCase {
         cars.setUser(user);
         em.persist(cars);
 
+        Category household = new Category();
+        household.setName("Household");
+        household.setUser(user);
+        em.persist(household);
+
         Resource bankAccount = new Resource();
         bankAccount.setCurrency(USD);
         bankAccount.setBalance(BigDecimal.ZERO);
@@ -82,21 +78,85 @@ public class StatisticsProviderImplTest extends TestCase {
         bankAccount.setUser(user);
         em.persist(bankAccount);
 
+        Resource bankLoan = new Resource();
+        bankLoan.setCurrency(USD);
+        bankLoan.setBalance(BigDecimal.ZERO);
+        bankLoan.setName("Bank Loan");
+        bankLoan.setUser(user);
+        em.persist(bankLoan);
+
         Transaction t1 = new Transaction();
         t1.setAmount(new BigDecimal(1000));
         t1.setCategory(cars);
         t1.setResource(bankAccount);
+        t1.setDate(LocalDate.now());
+        em.persist(t1);
 
         Transaction t2 = new Transaction();
-        t2.setAmount(new BigDecimal(750));
+        t2.setAmount(new BigDecimal(-750));
         t2.setCategory(cars);
-        t2.setResource(bankAccount);
+        t2.setResource(bankLoan);
+        t2.setDate(LocalDate.of(1994, 2, 28));
+        em.persist(t2);
 
+        Transaction t3 = new Transaction();
+        t3.setAmount(new BigDecimal(-450));
+        t3.setCategory(household);
+        t3.setResource(bankAccount);
+        t3.setDate(LocalDate.now());
+        em.persist(t3);
+
+        //------- 2nd User -------
 
         User anotherUser = new User();
         anotherUser.setUserName("mark tester");
         anotherUser.setDefaultCurrency(USD);
         em.persist(anotherUser);
+
+        Category cars2 = new Category();
+        cars2.setName("Cars");
+        cars2.setUser(user);
+        em.persist(cars2);
+
+        Category household2 = new Category();
+        household2.setName("Household");
+        household2.setUser(user);
+        em.persist(household2);
+
+        Resource bankAccount2 = new Resource();
+        bankAccount2.setCurrency(USD);
+        bankAccount2.setBalance(BigDecimal.ZERO);
+        bankAccount2.setName("Bank Account2");
+        bankAccount2.setUser(anotherUser);
+        em.persist(bankAccount2);
+
+        Resource bankLoan2 = new Resource();
+        bankLoan2.setCurrency(USD);
+        bankLoan2.setBalance(BigDecimal.ZERO);
+        bankLoan2.setName("Bank Loan2");
+        bankLoan2.setUser(anotherUser);
+        em.persist(bankLoan2);
+
+        Transaction t4 = new Transaction();
+        t4.setAmount(new BigDecimal(550));
+        t4.setCategory(cars2);
+        t4.setResource(bankAccount2);
+        t4.setDate(LocalDate.now());
+        em.persist(t4);
+
+        Transaction t5 = new Transaction();
+        t5.setAmount(new BigDecimal(-250));
+        t5.setCategory(cars2);
+        t5.setResource(bankLoan2);
+        t5.setDate(LocalDate.of(1992, 3, 18));
+        em.persist(t5);
+
+        Transaction t6 = new Transaction();
+        t6.setAmount(new BigDecimal(-350));
+        t6.setCategory(household2);
+        t6.setResource(bankAccount2);
+        t6.setDate(LocalDate.now());
+        em.persist(t6);
 
     }
 
