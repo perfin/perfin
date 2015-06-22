3f6f98364402f47c537f7a9e7f39c3d184c03494
Tomáš Livora <livoratom@gmail.com>
2015-06-22 22:20:44 +0200

    Add category view

---
+++ perfin-web/src/main/webapp/controller/category.js
@@ -0,0 +1,127 @@
+app.controller('categoryListController', function ($scope, $rootScope, categoryService) {
+    // Initialize required information: sorting, the first page to show and the grid options.
+    $scope.sortInfo = {fields: ['id'], directions: ['asc']};
+    $scope.categories = {currentPage: 1};
+
+    $scope.gridOptions = {
+        data: 'categories.list',
+        useExternalSorting: true,
+        sortInfo: $scope.sortInfo,
+
+        columnDefs: [
+            { field: 'id', displayName: 'Id' },
+            { field: 'name', displayName: 'Name' },
+            { field: '', width: 30, cellTemplate: '<span class="glyphicon glyphicon-remove remove" ng-click="deleteRow(row)"></span>' }
+        ],
+
+        multiSelect: false,
+        selectedItems: [],
+        // Broadcasts an event when a row is selected, to signal the form that it needs to load the row data.
+        afterSelectionChange: function (rowItem) {
+            if (rowItem.selected) {
+                $rootScope.$broadcast('categorySelected', $scope.gridOptions.selectedItems[0].id);
+            }
+        }
+    };
+
+    // Refresh the grid, calling the appropriate rest method.
+    $scope.refreshGrid = function () {
+        var listCategoryArgs = {
+            page: $scope.categories.currentPage,
+            sortFields: $scope.sortInfo.fields[0],
+            sortDirections: $scope.sortInfo.directions[0]
+        };
+
+        categoryService.get(listCategoryArgs, function (data) {
+            $scope.categories = data;
+        })
+    };
+
+    // Broadcast an event when an element in the grid is deleted. No real deletion is perfomed at this point.
+    $scope.deleteRow = function (row) {
+        $rootScope.$broadcast('deleteCategory', row.entity.id);
+    };
+
+    // Watch the sortInfo variable. If changes are detected than we need to refresh the grid.
+    // This also works for the first page access, since we assign the initial sorting in the initialize section.
+    $scope.$watch('sortInfo.fields[0]', function () {
+        $scope.refreshGrid();
+    }, true);
+
+    // Do something when the grid is sorted.
+    // The grid throws the ngGridEventSorted that gets picked up here and assigns the sortInfo to the scope.
+    // This will allow to watch the sortInfo in the scope for changed and refresh the grid.
+    $scope.$on('ngGridEventSorted', function (event, sortInfo) {
+        $scope.sortInfo = sortInfo;
+    });
+
+    // Picks the event broadcasted when a category is saved or deleted to refresh the grid elements with the most
+    // updated information.
+    $scope.$on('refreshGrid', function () {
+        $scope.refreshGrid();
+    });
+
+    // Picks the event broadcasted when the form is cleared to also clear the grid selection.
+    $scope.$on('clear', function () {
+        $scope.gridOptions.selectAll(false);
+    });
+});
+
+// Create a controller with name categoryFormController to bind to the form section.
+app.controller('categoryFormController', function ($scope, $rootScope, categoryService) {
+    // Clears the form. Either by clicking the 'Clear' button in the form, or when a successfull save is performed.
+    $scope.clearForm = function () {
+        $scope.category = null;
+        // For some reason, I was unable to clear field values with type 'url' if the value is invalid.
+        // This is a workaroud. Needs proper investigation.
+        document.getElementById('imageUrl').value = null;
+        // Resets the form validation state.
+        $scope.categoryForm.$setPristine();
+        // Broadcast the event to also clear the grid selection.
+        $rootScope.$broadcast('clear');
+    };
+
+    // Calls the rest method to save a category.
+    $scope.updateCategory = function () {
+        categoryService.save($scope.category).$promise.then(
+            function () {
+                // Broadcast the event to refresh the grid.
+                $rootScope.$broadcast('refreshGrid');
+                // Broadcast the event to display a save message.
+                $rootScope.$broadcast('categorySaved');
+                $scope.clearForm();
+            },
+            function () {
+                // Broadcast the event for a server error.
+                $rootScope.$broadcast('error');
+            });
+    };
+
+    // Picks up the event broadcasted when the category is selected from the grid and perform the category load by
+    // the appropiate rest service.
+    $scope.$on('categorySelected', function (event, id) {
+        $scope.category = categoryService.get({id: id});
+    });
+
+    // Picks us the event broadcasted when the category is deleted from the grid and perform the actual category delete
+    // by calling the appropiate rest service.
+    $scope.$on('deleteCategory', function (event, id) {
+        categoryService.delete({id: id}).$promise.then(
+            function () {
+                // Broadcast the event to refresh the grid.
+                $rootScope.$broadcast('refreshGrid');
+                // Broadcast the event to display a delete message.
+                $rootScope.$broadcast('categoryDeleted');
+                $scope.clearForm();
+            },
+            function () {
+                // Broadcast the event for a server error.
+                $rootScope.$broadcast('error');
+            });
+    });
+});
+
+// Service that provides categories operations
+app.factory('categoryService', function ($resource) {
+    return $resource('service/categories/:id');
+});
