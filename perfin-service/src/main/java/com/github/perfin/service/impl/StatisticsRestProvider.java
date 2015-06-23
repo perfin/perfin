91013b5f0c14876b319d194896f02c3cb87d7b37
Marián Macik <macik.marian@gmail.com>
2015-06-23 14:59:38 +0200

    StatisticsRestProvider almost complete.

--- perfin-service/src/main/java/com/github/perfin/service/impl/StatisticsRestProvider.java
+++ perfin-service/src/main/java/com/github/perfin/service/impl/StatisticsRestProvider.java
@@ -4,11 +4,11 @@ import com.github.perfin.service.api.StatisticsProvider;
 import com.github.perfin.service.dto.Statistics;
 
 import javax.inject.Inject;
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 /**
  * Simple wrapper, which acts as a REST provider for statistics.
@@ -21,9 +21,18 @@ public class StatisticsRestProvider {
     @Inject
     private StatisticsProvider statisticsProvider;
 
-    public Statistics getStatisticsByDateRange(LocalDate startDate, LocalDate endDate) {
-        Statistics statistics = null;
-        return statistics;
+    @GET
+    public Statistics getStatisticsByDateRange(@QueryParam("startDate") String startDate, @QueryParam("endDate") String endDate) {
+        LocalDate start;
+        LocalDate end;
+        try {
+            start = LocalDate.parse(startDate, DateTimeFormatter.BASIC_ISO_DATE);
+            end = LocalDate.parse(endDate, DateTimeFormatter.BASIC_ISO_DATE);
+        } catch (DateTimeParseException ex) {
+            throw new IllegalArgumentException("Date format is wrong!", ex);
+        }
+
+        return statisticsProvider.getStatisticsByDateRange(start, end);
     }
 
 }
