2162d9e7981cbbdc71bdf93556049ea59ac803e6
Tomáš Livora <livoratom@gmail.com>
2015-06-24 10:10:19 +0200

    Add EUR and CZK exchange rates to DB on startup

--- perfin-model/src/main/resources/data.sql
+++ perfin-model/src/main/resources/data.sql
@@ -1,5 +1,7 @@
 INSERT INTO Currency (code, name) VALUES ('AUD', ''), ('BGN', ''), ('BRL', ''), ('CAD', ''), ('CHF', ''), ('CNY', ''), ('CZK', ''), ('DKK', ''), ('EUR', ''), ('GBP', ''), ('HKD', ''), ('HRK', ''), ('HUF', ''), ('IDR', ''), ('ILS', ''), ('INR', ''), ('JPY', ''), ('KRW', ''), ('MXN', ''), ('MYR', ''), ('NOK', ''), ('NZD', ''), ('PHP', ''), ('PLN', ''), ('RON', ''), ('RUB', ''), ('SEK', ''), ('SGD', ''), ('THB', ''), ('TRY', ''), ('USD', ''), ('ZAR', '');
 
+INSERT INTO ExchangeRate (id, origin, target, ratio, date) VALUES (1, 7, 9, 0.0368, CURRENT_DATE()), (2, 9, 7, 27.189, CURRENT_DATE())
+
 INSERT INTO User (id, userName, currency) VALUES (1, 'jomarko', 9), (2, 'mmacik', 9), (3, 'tlivora', 7);
 
 INSERT INTO Category (id, name, user) VALUES (1, 'Food', 3), (2, 'Books', 3), (3, 'Sport', 3), (4, 'Salary', 3);
