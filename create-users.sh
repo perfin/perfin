634f5d45dd10441b4ed47c7d26903f563a7272a5
Jozef Marko <jomarko@redhat.com>
2015-06-24 23:22:38 +0200

    script for users creation

---
+++ create-users.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+./bin/add-user.sh jomarko User1234. -a -g admin,premium,standard
+./bin/add-user.sh tlivora User1234. -a -g admin,premium,standard
+./bin/add-user.sh mmacik User1234. -a -g admin,premium,standard
+./bin/add-user.sh JMSUser User1234. -a -g admin,premium
+./bin/add-user.sh adminuser User1234. -a -g admin
+./bin/add-user.sh premiumuser User1234. -a -g premium
+./bin/add-user.sh standarduser User1234. -a -g standard
+
